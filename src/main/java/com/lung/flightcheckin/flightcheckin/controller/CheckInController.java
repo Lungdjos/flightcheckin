5f04b50a561133a99536a0dbbfd6b72fa51b9b69
Lungdjos <lungudjos@gmail.com>
2023-03-06 12:18:09 +0200

    creating a start chekin method in the controller

--- src/main/java/com/lung/flightcheckin/flightcheckin/controller/CheckInController.java
+++ src/main/java/com/lung/flightcheckin/flightcheckin/controller/CheckInController.java
@@ -1,15 +1,30 @@
 package com.lung.flightcheckin.flightcheckin.controller;
 
+import com.lung.flightcheckin.flightcheckin.integration.ReservationRestClient;
+import com.lung.flightcheckin.flightcheckin.integration.dto.Reservation;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 
 @Controller
 public class CheckInController {
+    @Autowired
+    private ReservationRestClient reservationRestClient;
 
     // method to show the Checking page
     @RequestMapping("/showCheckIn")
-    public String startCheckIn(){
+    public String showCheckIn(){
         return "startCheckIn";
+    }
+
+    // method handling the start check in
+    @RequestMapping("/startCheckIn")
+    public String startCheckIn(@RequestParam("reservationId") int reservationId, ModelMap modelMap){
+        Reservation reservation = reservationRestClient.findReservation(reservationId);
+        modelMap.addAttribute("reservation", reservation);
+        return "displayReservationDetails";
     }
 }
--- src/main/java/com/lung/flightcheckin/flightcheckin/integration/implementation/ReservationRestClientImpl.java
+++ src/main/java/com/lung/flightcheckin/flightcheckin/integration/implementation/ReservationRestClientImpl.java
@@ -3,8 +3,10 @@ package com.lung.flightcheckin.flightcheckin.integration.implementation;
 import com.lung.flightcheckin.flightcheckin.integration.ReservationRestClient;
 import com.lung.flightcheckin.flightcheckin.integration.dto.Reservation;
 import com.lung.flightcheckin.flightcheckin.integration.dto.ReservationUpdateRequest;
+import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
+@Component
 public class ReservationRestClientImpl implements ReservationRestClient {
     // the url static variable
     private static final String RESERVATION_REST_URL = "http://localhost:8080/flightreservationmgt/";
